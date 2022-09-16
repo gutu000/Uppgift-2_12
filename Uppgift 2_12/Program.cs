62b92a4cc95ee635033eed0ff98fbbf5a72bc69d
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-16 14:22:38 +0200

    Hej

--- Uppgift 2_12/Program.cs
+++ Uppgift 2_12/Program.cs
@@ -7,17 +7,17 @@ namespace Uppgift_2._1
         static void Main(string[] args)
         {
             Console.WriteLine("Hur många dagar vill du hyra bilen?");
-            Console.ReadLine();
-            Console.WriteLine("Okej och hur många kilometer?");
-            string dagar = Console.ReadLine();
-            int dagtotal = 500;
-            int kilometerpris = 40;
-            int start = 300;
-            int allt = dagtotal + kilometerpris + start;
+            string  strNr = Console.ReadLine();
+            int nr = Convert.ToInt32(strNr);
+            nr = nr + - 1;
+            int rr = nr * 500 + 300;
+            Console.WriteLine("Okej hur många kilometer ska du köra?");
+            String strKm = Console.ReadLine();
+            int km = Convert.ToInt32(strKm);
+            km = km;
+            Console.Write("Så här mycket kommer det att kosta");
+            Console.WriteLine(rr + km);
 
-            Console.WriteLine("Okej, det kommer kosta totalt " + allt);
-            // hej
-            
         }
     }
 }
